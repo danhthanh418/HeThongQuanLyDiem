7d500fcb4040ea800f2eadb3d0f5072d84f0a533
Thanh <15520798@gm.uit.edu.vn>
2018-06-07 09:40:00 +0700

    update XuLy.cs

---
+++ HeThongQuanLyDiem/DataAccess/XyLy.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data;
+
+namespace HeThongQuanLyDiem.DataAccess
+{
+	class XyLy
+	{
+		//Tạo chuỗi kết nối
+		SqlConnection conn = new SqlConnection(ConfigurationSettings.AppSettings["StringConn"]);
+		//Kết nối đổ dữ liệu vào data table
+		SqlDataAdapter adapter;
+		//Xử lý dữ liệu IUD;
+		SqlCommand cmd;
+
+		public DataTable table_Select(String sql)
+		{
+			adapter = new SqlDataAdapter(sql, conn);
+			//Tạo mới table
+			var table = new DataTable();
+			//Đỗ dữ lêiuj vào
+			adapter.Fill(table);
+			//Trả về data table
+			return table;
+		}
+
+		public void table_Command(String sql)
+		{
+			conn.Open();
+			//Khỏi tạo biến command với 2 parameter sql,conn
+			cmd = new SqlCommand(sql,conn);
+			//Thực thi câu lệnh	
+			cmd.ExecuteNonQuery();
+			cmd.Dispose();
+			//Đóng kết nối
+			conn.Close();
+		}
+	}
+}
